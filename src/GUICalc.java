83d5c0befc4172487943ebc70e6d0e5a4997c14f
Sohan Kabiraj <32105731+sohankabiraj@users.noreply.github.com>
2017-10-18 22:51:06 -0400

    Adding functionality
    Updating and providing functions for the buttons. Still need to do the Calculate portion.

--- src/GUICalc.java
+++ src/GUICalc.java
@@ -8,11 +8,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JScrollPane;
@@ -75,8 +83,6 @@ public class GUICalc extends JFrame {
 		
 		JButton btnRemoveAll = new JButton("Remove All ");
 		
-		JButton btnUpdateSelected = new JButton("Update Selected");
-		
 		JLabel lblNewLabel = new JLabel("Current G.P.A.:");
 		
 		JLabel lblRequiredGpa = new JLabel("Required G.P.A.:");
@@ -97,57 +103,60 @@ public class GUICalc extends JFrame {
 		
 		JTextArea textGrade = new JTextArea();
 		
+		JButton btnCalculate = new JButton("Calculate");
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addGroup(gl_contentPane.createSequentialGroup()
-											.addGap(56)
-											.addComponent(lblGpa))
-										.addGroup(gl_contentPane.createSequentialGroup()
-											.addContainerGap()
-											.addComponent(lblCreditHours))
-										.addGroup(gl_contentPane.createSequentialGroup()
-											.addContainerGap()
-											.addComponent(lblGpa_1, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)))
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(textCreditHours, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-										.addComponent(textGPA, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-										.addComponent(textTargetGPA, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-										.addComponent(textCourseName, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(btnAddButton)
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(btnAddBlank, GroupLayout.PREFERRED_SIZE, 122, Short.MAX_VALUE)))
-							.addPreferredGap(ComponentPlacement.RELATED, 17, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnUpdateSelected, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnRemoveSelected, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED))
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+											.addGroup(gl_contentPane.createSequentialGroup()
+												.addGap(56)
+												.addComponent(lblGpa))
+											.addGroup(gl_contentPane.createSequentialGroup()
+												.addContainerGap()
+												.addComponent(lblCreditHours))
+											.addGroup(gl_contentPane.createSequentialGroup()
+												.addContainerGap()
+												.addComponent(lblGpa_1, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)))
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+											.addComponent(textCreditHours, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+											.addComponent(textGPA, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+											.addComponent(textTargetGPA, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+											.addComponent(textCourseName, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)))
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(btnAddButton)
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addComponent(btnAddBlank, GroupLayout.PREFERRED_SIZE, 122, Short.MAX_VALUE)))
+								.addPreferredGap(ComponentPlacement.RELATED, 17, GroupLayout.PREFERRED_SIZE))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(lblTargetGpa, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
+								.addPreferredGap(ComponentPlacement.RELATED))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(lblGrade, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(textGrade, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(lblCourseName, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
+								.addPreferredGap(ComponentPlacement.RELATED))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(btnRemoveSelected, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(btnCalculate)
+								.addPreferredGap(ComponentPlacement.RELATED)))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addComponent(btnRemoveAll, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblTargetGpa, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
-							.addPreferredGap(ComponentPlacement.RELATED))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblGrade, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textGrade, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblCourseName, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)))
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 358, GroupLayout.PREFERRED_SIZE)
@@ -165,50 +174,54 @@ public class GUICalc extends JFrame {
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 377, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblGpa)
-							.addGap(31)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblCreditHours)
-								.addComponent(textCreditHours, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(13)
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 377, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(lblGpa)
+									.addGap(31)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(lblCreditHours)
+										.addComponent(textCreditHours, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGap(13)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(lblGpa_1)
+										.addComponent(textGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addPreferredGap(ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(lblGrade)
+										.addComponent(textGrade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGap(14)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(lblCourseName)
+										.addComponent(textCourseName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGap(14)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(lblTargetGpa)
+										.addComponent(textTargetGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGap(18)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(btnAddButton)
+										.addComponent(btnAddBlank))
+									.addGap(20)
+									.addComponent(btnRemoveSelected)
+									.addGap(15)
+									.addComponent(btnRemoveAll)
+									.addGap(51)))
+							.addGap(27)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblGpa_1)
-								.addComponent(textGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(lblNewLabel)
+								.addComponent(textCurrentGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblGrade)
-								.addComponent(textGrade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(14)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblCourseName)
-								.addComponent(textCourseName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(14)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblTargetGpa)
-								.addComponent(textTargetGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(18)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnAddButton)
-								.addComponent(btnAddBlank))
-							.addGap(21)
-							.addComponent(btnUpdateSelected)
-							.addGap(19)
-							.addComponent(btnRemoveSelected)
-							.addGap(17)
-							.addComponent(btnRemoveAll)))
-					.addGap(27)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel)
-						.addComponent(textCurrentGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textRequiredGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblRequiredGpa))
-					.addGap(108))
+								.addComponent(textRequiredGPA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblRequiredGpa))
+							.addGap(108))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnCalculate)
+							.addGap(183))))
 		);
 		
 		table = new JTable();
@@ -221,18 +234,68 @@ public class GUICalc extends JFrame {
 		table.setModel(model);
 		
 		Object[] row = new Object[4];
+		ArrayList<Integer> creditHours = new ArrayList<Integer>();
+		ArrayList<Float> gpas = new ArrayList<Float>(); 		
 		btnAddButton.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+								
+				if (textGrade.getText().equals("A+")) {
+					textGPA.setText(String.valueOf(4.000));
+				}
+				if (textGrade.getText().equals("A")) {
+					textGPA.setText(String.valueOf(4.000));
+				}
+				if (textGrade.getText().equals("A-")) {
+					textGPA.setText(String.valueOf(3.700));
+				}
+				if (textGrade.getText().equals("B+")) {
+					textGPA.setText(String.valueOf(3.300));
+				}
+				if (textGrade.getText().equals("B")) {
+					textGPA.setText(String.valueOf(3.000));
+				}
+				if (textGrade.getText().equals("B-")) {
+					textGPA.setText(String.valueOf(2.700));
+				}
+				if (textGrade.getText().equals("C+")) {
+					textGPA.setText(String.valueOf(2.300));
+				}
+				if (textGrade.getText().equals("C")) {
+					textGPA.setText(String.valueOf(2.000));
+				}
+				if (textGrade.getText().equals("C-")) {
+					textGPA.setText(String.valueOf(1.700));
+				}
+				if (textGrade.getText().equals("D+")) {
+					textGPA.setText(String.valueOf(1.300));
+				}
+				if (textGrade.getText().equals("D")) {
+					textGPA.setText(String.valueOf(1.000));
+				}
+				if (textGrade.getText().equals("D-")) {
+					textGPA.setText(String.valueOf(0.700));
+				}
+				if (textGrade.getText().equals("F")) {
+					textGPA.setText(String.valueOf(0.000));
+				}
 				
+
 				row[0] = textCreditHours.getText();
 				row[1] = textGPA.getText();
 				row[2] = textGrade.getText();
 				row[3] = textCourseName.getText();
 				
 				model.addRow(row);
-
+				
+				int getCredit = Integer.parseInt(textCreditHours.getText());
+				creditHours.add(getCredit);
+				float getGPA = Float.parseFloat(textGPA.getText());
+				gpas.add(getGPA);
+				
+				System.out.print(Arrays.toString(creditHours.toArray()));
+				System.out.println(Arrays.toString(gpas.toArray()));
 			}
 		});
 		
@@ -243,6 +306,8 @@ public class GUICalc extends JFrame {
 				int i = table.getSelectedRow();
 				if (i >= 0) {
 					model.removeRow(i);
+					creditHours.remove(i);
+					gpas.remove(i);
 				}
 				else {
 					JOptionPane.showMessageDialog(contentPane, "Nothing selected to remove!");
@@ -250,6 +315,17 @@ public class GUICalc extends JFrame {
 			}
 		});
 		
+		btnRemoveAll.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = model.getRowCount() - 1; i >= 0; i--) {
+					model.removeRow(i);
+					creditHours.remove(i);
+					gpas.remove(i);
+				}
+			}
+		});
 		table.addMouseListener(new MouseListener() {
 
 			@Override
@@ -287,24 +363,16 @@ public class GUICalc extends JFrame {
 			}
 		});
 		
-		btnUpdateSelected.addActionListener(new ActionListener() {
+		btnCalculate.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				int i = table.getSelectedRow();
-				
-				if(i >= 0) {
-					model.setValueAt(textCreditHours.getText(), i, 0);
-					model.setValueAt(textGPA.getText(), i, 1);
-					model.setValueAt(textGrade.getText(), i, 2);
-					model.setValueAt(textCourseName.getText(), i, 3);	
-				}
-				else {
-					JOptionPane.showMessageDialog(contentPane, "Nothing selected to update!");
-				}
-
 			}
 		});
+	}
+	
+	public static void getData() {
+		
 	}
 }
